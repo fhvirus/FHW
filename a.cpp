02491b30e313548e2032d9c43a4347a85fdc34ad
FHVirus <froghackervirus@gmail.com>
2023-02-20 17:05:19 +0800

    Initial commit

---
+++ a.cpp
@@ -0,0 +1,15 @@
+// This line will not be shown.
+#include <bits/stdc++.h>
+#define MAXN 7122
+
+int main() {
+	std::cin.tie(0)->sync_with_stdio(0);
+	std::cin.exceptions(std::cin.failbit);
+
+	int a = 998244353;
+	const char c = 'x';
+
+	std::cout << "Hello world!" << std::endl;
+
+	return 0;
+}
